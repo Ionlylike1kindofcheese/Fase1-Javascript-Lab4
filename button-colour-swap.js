f7be1092b8dee421d6c1e78d0c5e0f2696ca4ec4
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2023-03-06 15:06:35 +0100

    Lab 4 - Button Colours V1 (done)

---
+++ button-colour-swap.js
@@ -0,0 +1,27 @@
+function colourSwap(indexNumber) {
+  document.getElementById(indexNumber).style.backgroundColor = 'red';
+}
+
+// Setup begin
+let buttonContainer = document.getElementById('container');
+buttonContainer.style.width = '80%';
+buttonContainer.style.backgroundColor = 'grey';
+buttonContainer.style.margin = 'auto';
+buttonContainer.style.display = 'flex';
+buttonContainer.style.flexWrap = 'wrap';
+buttonContainer.style.justifyContent = 'center';
+buttonContainer.style.padding = '0.5em';
+for (let index = 1; index <= 30; index++) {
+  let button = document.createElement('button');
+  button.style.background = 'green';
+  button.style.margin = '0.5em';
+  button.style.width = '18%';
+  button.style.padding = '1.5em';
+  button.style.textAlign = 'center';
+  button.innerText = index;
+  button.style.fontWeight = 'bold';
+  button.id = `button-${index}`;
+  button.addEventListener('click', colourSwap.bind(undefined, `button-${index}`));
+  buttonContainer.appendChild(button);
+}
+// Setup end
